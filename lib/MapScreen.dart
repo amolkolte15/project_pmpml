696afa262575aa41b96f15db022aa2e640f8083e
Amol kolte <tachnicalbapu2003@gmail.com>
2025-01-23 16:22:25 +0530

    Add Mobile Authentication

--- lib/MapScreen.dart
+++ lib/MapScreen.dart
@@ -97,4 +97,3 @@ class _MapScreenState extends State<MapScreen> {
   }
 }
 
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:pmpml_app/phoneAuth.dart';
 import 'MapScreen.dart';
 
 void main() async {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MapScreen(),
+      home: LoginScreen(),
     );
   }
 }
